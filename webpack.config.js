8426561e32cdba3fff98d495f0592e7e9c6562e1
Pio <aadios56@gmail.com>
2022-10-27 10:02:57 +0300

    Little more to go (still develoing)

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const postCSSPlugins=[
+    require('postcss-import'),
+    require ('postcss-simple-vars'),
+    require ('postcss-nested'),
+    require('autoprefixer')
+]
+
+module.exports={
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname,'app')
+    },
+    devServer: {
+        static: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0',
+        historyApiFallback: { index: "index.html" }
+      },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader',{loader: 'postcss-loader', options: {postcssOptions:{plugins: postCSSPlugins}}}]
+            }
+        ]
+    }
+}
