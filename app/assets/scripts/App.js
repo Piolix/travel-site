94a5a9764fbc0be1b23ee5d3ff3440d1f3a43850
Pio <aadios56@gmail.com>
2022-11-08 22:47:25 +0300

    header and modal completed!

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,29 @@
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from  './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader';
+import 'lazysizes'
 
-if(module.hot){
-    module.hot.accept();
+let stickyHeader = new StickyHeader();
+let mobileMenu = new MobileMenu();
+let modal
+
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if(typeof modal == "undefined"){
+      import(/* webpackChunkName: "Modal" */'./modules/Modal').then( x => {
+        modal=  new x.default()
+        setTimeout(() => modal.openTheModal(),20)
+      }).catch(()=> console.log("problemm"))
+    }else{
+      modal.openTheModal()
+    }
+  })
+})
+
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial-item__bgwhite"), 60)
+if (module.hot) {
+  module.hot.accept()
 }
